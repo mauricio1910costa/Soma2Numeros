fcbab5f385113a2963405c36ae2a8f2ed3f39d38
Mauricio Costa <mauriciof1910@hotmail.com>
2020-09-18 16:26:13 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cálculo da soma entre dois números");
+            int M = 30;
+            int A = 40;
+            int Soma = M + A;
+            Console.WriteLine($"{M} + {A} = {Soma}");
+        }
+    }
+}
